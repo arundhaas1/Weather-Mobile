dd6ce5390a3a11213d96577d544b6df2ef9e13d3
Arundhaas1 <arunram.9215@gmail.com>
2021-06-04 14:50:00 +0530

    adding styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react'
 import './App.css';
+import 'tachyons'
+
 
 function App() {
   return (
-    <div>
-    
+    <div className="app">
+        <div className="searchBox tc ">
+          <input type="text" placeholder="Search..." className="Search" />
+        </div>          
+        <div className="place tc">
+           TamilNadu
+        </div>
+        <div className="date tc">
+          28th March
+        </div>
+        <div className="temperature tc">
+          15&#8451;
+        </div>
+        <div className="climate tc">
+          Cloudy
+        </div>
     </div>
   );
 }
